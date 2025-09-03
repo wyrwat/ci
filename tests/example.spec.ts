0497f8cbbd9e8a5b90d1327f99b6d5efa1bcf81c
wyrwat <wyrwa.t@gmail.com>
2025-09-03 09:02:39 +0200

    refactor workflows

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title @smoke', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
